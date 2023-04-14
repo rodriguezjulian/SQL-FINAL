33195685482000ff668b314293b0d2f572aaeb71
rodriguezjulian <julian.rodriguez.t2@gmail.com>
2023-04-13 21:00:49 -0300

    Vistas cliente - vendedor - articulo - orden

--- vistas.sql
+++ vistas.sql
@@ -1,10 +1,106 @@
-/*CREATE VIEW vw_Clientes AS -- creo la vista
-SELECT*
-FROM cliente;*/
+use proyecto_final;
+
+CREATE VIEW vw_Clientes AS
+(select idCliente, nombre, cuit_cuil, case when idMetodoPago = 1 then 'Credito VISA' 
+	  when idCondicionFiscal = 2 then 'Debito VISA' 
+      when idCondicionFiscal = 3 then 'Credito Mastercard'
+     when idCondicionFiscal = 4 then 'Cripto Binance' end Metodo_Pago
+from cliente) ;
 /*
-SELECT  * FROM vw_Clientes; -- llamo a la vista.
+-- FUNCIONA
+ CREATE VIEW vw_ClientesV2 AS
+SELECT C.idCliente, C.nombre, C.cuit_cuil,Z.descripcionZonaGeografica
+FROM cliente AS C
+INNER JOIN zonaGeografica AS Z
+ON C.idZonaGeografica = Z.idZonaGeografica
+;
+SELECT  * FROM vw_Clientes;*/
+
+CREATE VIEW vw_ClientesV3 AS
+SELECT C.idCliente, C.nombre, C.cuit_cuil,Z.descripcionZonaGeografica , CF.descripcionCondicionFiscal, MP.metodo,MP.descripcion
+FROM cliente AS C
+INNER JOIN zonaGeografica AS Z
+ON C.idZonaGeografica = Z.idZonaGeografica
+INNER JOIN condificionFiscal AS CF
+ON C.idCondicionFiscal = CF.idCondicionFiscal
+INNER JOIN metodosPago AS MP
+ON C.idMetodoPago = MP.id
+order by idCliente asc;
+;
+
+CREATE VIEW vw_VendedoresV1 AS
+SELECT vendId ,vendRazonSocial ,vendCuit 
+FROM vendedor
+order by vendId asc;
+
+CREATE VIEW vw_ArticulosV1 AS
+SELECT A.idArticulo, A.descripcionArticulo , A.color , A.stock ,A.precio , V.vendId, V.vendRazonSocial
+FROM articulo AS A
+INNER JOIN vendedor AS V
+ON A.vendId = V.vendId
+order by idArticulo asc;
+
+CREATE VIEW vw_ordenes AS
+SELECT O.idOrden ,C.nombre  ,V.vendId, V.vendRazonSocial, A.descripcionArticulo , O.unidades
+FROM orden AS O
+INNER JOIN  cliente AS C
+ON O.idCliente = C.idCliente 
+INNER JOIN  vendedor AS V
+ON O.idVendedor  = V.vendId
+INNER JOIN  articulo AS A
+ON O.idArticulo   = A.idArticulo 
+order by idOrden asc;
+;
+
+
+
+
+SELECT  * FROM vw_ClientesV3;
+SELECT  * FROM vw_VendedoresV1;
+SELECT  * FROM vw_ArticulosV1;
+SELECT  * FROM vw_ordenes;
+
+
+/*DROP VIEW vw_ClientesV3;
+DROP VIEW vw_VendedoresV1;
+DROP VIEW vw_ArticulosV1;
+DROP VIEW  vw_ordenes;*/
+
+
+-- ============================================================================================================================================================
+
+
+
+
+
+
+/*(select case when idCondicionFiscal = 1 then 'Credito VISA' 
+	  when idCondicionFiscal = 2 then 'Debito VISA' 
+      when idCondicionFiscal = 3 then 'Credito Mastercard'
+     when idCondicionFiscal = 4 then 'Cripto Binance' from condificionFiscal) Condicion_Fiscal*/
+
+-- group by id_level
+-- order by id_level asc;
+
+/*select id_level,
+(case when id_level = 1 then 'Primer valor' 
+	  when id_level = 2 then 'segundo valor' 
+      when id_level = 3 then 'tercer valor'
+      else 'otros valores' end) id_level_desc  
+from game
+group by id_level
+order by id_level asc;
+select* from cliente limit 3;*/
+/*
 CREATE OR REPLACE VIEW vw_Clientes AS
-(select idCliente, nombre, cuit_cuil
-from cliente);
-SELECT  * FROM vw_Clientes; -- llamo a la vista.
+	SELECT idCliente, nombre, cuit_cuil
+    FROM zonaGeografica AS ZG
+    INNER JOIN regions AS r
+    ON t.region_id = r.region_id;
+*/
+/*CREATE OR REPLACE VIEW vw_regiones AS
+	SELECT t.team_id, t.team_name, r.region_name 
+    FROM teams AS t
+    INNER JOIN regions AS r
+    ON t.region_id = r.region_id;
 */
